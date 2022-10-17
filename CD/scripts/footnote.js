46237d6390189d9380d1cda57609836b17aa0f97
Jannetta Steyn <6432530+jsteyn@users.noreply.github.com>
2022-10-17 22:18:40 +0200

    Nuutste byvoegings

--- CD/scripts/footnote.js
+++ CD/scripts/footnote.js
@@ -1,2 +1,2 @@
 document.write("<hr color=navy>");
-document.write("<p class='Voetnota'>The contents of this page belongs to the Peter Henrich Henning Family League (2020)</p>");
+document.write("<p class='Voetnota'>The contents of this page belongs to the Peter Henrich Henning Family League (2022)</p>");
